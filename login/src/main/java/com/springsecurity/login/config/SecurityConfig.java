a0f1cba3d4371b4175fa92f4981d7dad86dc313c
Martin Bjervig <martinbjervig@gmail.com>
2025-01-06 18:59:12 +0100

    changed my mind, i deleted BCrypto, for so long

--- login/src/main/java/com/springsecurity/login/config/SecurityConfig.java
+++ login/src/main/java/com/springsecurity/login/config/SecurityConfig.java
@@ -3,17 +3,15 @@ package com.springsecurity.login.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     // BCrypt krypterar lösenordet
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
     // säkerhetsinställningar
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
--- login/src/main/java/com/springsecurity/login/service/UserService.java
+++ login/src/main/java/com/springsecurity/login/service/UserService.java
@@ -1,7 +1,6 @@
 package com.springsecurity.login.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.springsecurity.login.entity.User;
@@ -12,19 +11,16 @@ public class UserService {
 
     
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
 
-    @Autowired // här ska en parameter för BCryptPasswordEncoder in
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    @Autowired // här ska en parameter för BCryptPasswordEncoder in...kanske
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+        
     }
 
 
     // metod för att registrera en användare 
     public User registerNewUser(User user) {
-        // krypterar lösenordet
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
         // användaren läggs till i databasen
         return userRepository.save(user);
     }
