812214a6ff7162321a384648647ad20114afacdc
Martin Bjervig <martinbjervig@gmail.com>
2025-01-06 15:00:51 +0100

    login/logout buttons now work, but´'myorder' is not redirecting

--- login/src/main/java/com/springsecurity/login/config/SecurityConfig.java
+++ login/src/main/java/com/springsecurity/login/config/SecurityConfig.java
@@ -13,63 +13,34 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests(auth -> auth
                 // kräver ingen inloggning
-                .requestMatchers( "/register", "/product/**", "/").permitAll()
+                .requestMatchers( "/", "/register", "/product/**").permitAll()
                 // kräver inloggning 
                 .requestMatchers("/myorder").authenticated() 
-                // resten av alla requests är tillgängligt för alla (i detta fall bara logga in/logga ut)
+                // resten av alla requests är tillgängligt för alla 
                 .anyRequest().permitAll()
             )
-            //här ska det finnas en inställning för att omdirigera användaren till startsidan(index) om åtkomst nekas
-            .exceptionHandling(exception -> exception
-                .authenticationEntryPoint((httpServletRequest, httpServletResponse, authenticationException) -> 
-                    httpServletResponse.sendRedirect("/"))
-                )
+            
             // här är inställningar för login/logout
             .formLogin(form -> form
-                .defaultSuccessUrl("/")
+                .defaultSuccessUrl("/", true)
                 .permitAll()
             )
             .logout(logout -> logout
                 .logoutSuccessUrl("/")
                 .permitAll()
             );
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            // .exceptionHandling(exception -> exception
-            //     .accessDeniedHandler((request, response, accessDeniedException) -> {
-            //         response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
-            //         response.setHeader("Location", "/");
-            //     })
-            // );
 
+            // .exceptionHandling(exception -> exception
+            //     .authenticationEntryPoint((request, response, authException) -> {
+            //         if (request.getRequestURI().equals("/myorder")) {
+            //             response.sendRedirect("/");
+            //         }
+            //     })
+            // )
+                
 
             
-            // vid nekad åtkomst skickas användaren till startsidan(index)
-            // .exceptionHandling(exception -> exception
-            // .accessDeniedHandler((request, response, accessDeniedException) -> {
-            //     System.out.println("Torsten Flink");
-            //     response.sendRedirect("/");
-            //     })
-            // );
-            
-            
-            
+
             
         return http.build();
                 
