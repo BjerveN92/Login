76d42e6f555dda9fe507512afc3b906edbf04948
Martin Bjervig <martinbjervig@gmail.com>
2025-01-07 14:11:28 +0100

    many big changes here, a registerform in HTML is done+config

--- login/src/main/java/com/springsecurity/login/repository/UserRepository.java
+++ login/src/main/java/com/springsecurity/login/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.springsecurity.login.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springsecurity.login.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional <User> findByUsername(String username);
 }
--- login/src/main/java/com/springsecurity/login/service/UserService.java
+++ login/src/main/java/com/springsecurity/login/service/UserService.java
@@ -1,13 +1,16 @@
 package com.springsecurity.login.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.springsecurity.login.entity.User;
 import com.springsecurity.login.repository.UserRepository;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     
     private final UserRepository userRepository;
@@ -17,12 +20,21 @@ public class UserService {
         this.userRepository = userRepository;
         
     }
-
-
     // metod för att registrera en användare 
-    public User registerNewUser(User user) {
+    public User saveUser(User user) {
         // användaren läggs till i databasen
         return userRepository.save(user);
+    }
+    // metod för att hitta en användare
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found")); 
+
+        return org.springframework.security.core.userdetails.User
+            .withUsername(user.getUsername())
+            .password(user.getPassword())
+            .build();
     }
     
 }
