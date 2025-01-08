cd60390c342b30e30bdbf82e9e5dd8f78695f0a5
Martin Bjervig <martinbjervig@gmail.com>
2025-01-08 10:18:03 +0100

    added 'more info' button to product

--- login/src/main/java/com/springsecurity/login/controller/ProductController.java
+++ login/src/main/java/com/springsecurity/login/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.springsecurity.login.controller;
 
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.springsecurity.login.entity.Product;
 import com.springsecurity.login.service.ProductService;
 
 @Controller
@@ -22,6 +26,8 @@ public class ProductController {
 
     @GetMapping("/product/{id}")
     public String productPage (@PathVariable Long id, Model model) {
+        Optional<Product> product = ProductService.getProductById(id);
+        model.addAttribute("product", product.get());
         return "product-details";
     }
     
